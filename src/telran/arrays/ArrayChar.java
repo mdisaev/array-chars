a9f26264406465d232682c241de61a55e0e43f80
michaelisaev <mdisaev@gmail.com>
2024-03-09 20:24:50 +0200

    ArrayChar compareTo test

---
+++ src/telran/arrays/ArrayChar.java
@@ -0,0 +1,12 @@
+package telran.arrays;
+
+public class ArrayChar {
+	private char[] array;
+	public ArrayChar(char[] array) {
+		this.array = array;
+	}
+ public int compareTo(ArrayChar another) {
+	 //TODO
+	 return -1; 
+ }
+}
